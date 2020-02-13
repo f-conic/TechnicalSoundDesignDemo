6e0053aff8244e1feae89a6c5a58984734b46671
Filip Conic <filip.conic@rovio.com>
2020-02-13 17:08:06 +0100

    Added first iteration of WorldGeometryBuilder to instantiate objects at runtime

---
+++ TSD_UnityProject/Assets/WorldGeometryBuilder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGeometryBuilder : MonoBehaviour
+{
+	private GameObject cube;
+
+	void Update()
+    {
+	    if (Input.GetKeyDown(KeyCode.B))
+	    {
+		    cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			cube.transform.position = transform.position + (transform.forward * 2);
+		    var surfaceReflector = cube.AddComponent<AkSurfaceReflector>();
+		    var aKGameObject = cube.AddComponent<AkGameObj>();
+	    }
+
+	    if (cube != null)
+	    {
+		    var mouseAxis = Input.GetAxis("Mouse ScrollWheel");
+		    cube.transform.localScale = cube.transform.localScale + new Vector3(1 * mouseAxis, 1 * mouseAxis, 0);
+	    }
+	}
+}
