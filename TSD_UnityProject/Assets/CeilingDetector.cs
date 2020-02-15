b6ea8106e660e279e103d4e55976d5063b2bc9d8
Filip Conic <mail@filipconic.com>
2020-02-15 21:50:22 +0100

    Created first iteration of ceiling detector. Changed obsolete code in FreeLookCamera.

---
+++ TSD_UnityProject/Assets/CeilingDetector.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CeilingDetector : MonoBehaviour
+{
+	private RaycastHit hit;
+	private int lastGameObjectId;
+
+	private void LateUpdate()
+	{
+		// There's probably a better way of doing this.
+		if (Camera.main != null)
+		{
+			gameObject.transform.position = Camera.main.transform.position;
+		}
+
+		if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.up), out hit, 100f))
+		{
+			var gameObjectId = hit.transform.gameObject.GetInstanceID();
+			if (lastGameObjectId != gameObjectId)
+			{
+				if (hit.transform.GetComponent<BoxCollider>() == null)
+				{
+					CreateCollider(hit);
+				}
+
+				lastGameObjectId = gameObjectId;
+			}
+		}
+	}
+
+	private void CreateCollider(RaycastHit hitObject)
+	{
+		var room = new GameObject("Room");
+		room.gameObject.transform.SetParent(hitObject.transform, false);
+		
+		var roomCollider = room.AddComponent<BoxCollider>();
+		var distanceToFloor = GetDistanceToFloor(hitObject);
+
+		roomCollider.size = new Vector3(1f, 1f, distanceToFloor);
+		roomCollider.center = new Vector3(0f, 0f, hitObject.transform.position.y - distanceToFloor/2f);
+
+		Debug.Log("<color=green>Found ceiling! Creating room.</color>");
+	}
+
+	private float GetDistanceToFloor(RaycastHit ceiling)
+	{
+		// This is fine, we only do it once.
+		var floor = GameObject.FindWithTag("Floor");
+
+		if (floor != null)
+		{
+			var distance = Vector3.Distance(floor.transform.position, ceiling.transform.position);
+			return distance;
+		}
+
+		return 0f;
+	}
+}
---
+++ TSD_UnityProject/Assets/TestTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTrigger : MonoBehaviour
+{
+	public AK.Wwise.Event DialogEvent;
+
+    void Start()
+    {
+	    AkSoundEngine.RegisterGameObj(gameObject);
+	    DialogEvent.Post(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
