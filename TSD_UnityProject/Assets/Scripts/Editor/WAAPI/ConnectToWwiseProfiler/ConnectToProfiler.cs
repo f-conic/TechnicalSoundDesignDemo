fc1859bd8aab90a156a297bafafc8632030f1b45
Filip Conic <filip.conic@rovio.com>
2020-02-17 14:03:28 +0100

    Created a tool for quicker profiler connection using WAAPI

---
+++ TSD_UnityProject/Assets/Scripts/Editor/WAAPI/ConnectToWwiseProfiler/ConnectToProfiler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using UnityEngine;
+
+public class ConnectToProfiler
+{
+	public static void Connect()
+	{
+		try
+		{
+			var dataPath = Application.dataPath + @"/Scripts/Editor/WAAPI/ConnectToWwiseProfiler/ConnectToProfiler.bat";
+			var process = new Process();
+			process.StartInfo.FileName = dataPath;
+			process.Start();
+		}
+		catch (Exception e)
+		{
+			UnityEngine.Debug.LogError(e);
+		}
+	} 
+}
