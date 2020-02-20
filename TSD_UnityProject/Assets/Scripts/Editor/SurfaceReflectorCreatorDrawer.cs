4141703dedfff7df76aac40beb492d61dadf293a
Filip Conic <filip.conic@rovio.com>
2020-02-20 17:28:14 +0100

    Modified main scene. Created a tool for creating surface reflectors.

---
+++ TSD_UnityProject/Assets/Scripts/Editor/SurfaceReflectorCreatorDrawer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(SurfaceReflectorCreator))]
+public class SurfaceReflectorCreatorDrawer : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		SurfaceReflectorCreator surfaceCreator = (SurfaceReflectorCreator) target;
+		DrawDefaultInspector();
+
+		if (GUILayout.Button("Make Surface Reflector"))
+		{
+			surfaceCreator.CreateSurfaceReflector();
+		}
+	}
+}
