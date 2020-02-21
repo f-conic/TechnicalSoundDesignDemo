70b39b592092a90256be970d66809eaea3ce2971
Filip Conic <filip.conic@rovio.com>
2020-02-21 09:27:37 +0100

    Material will now change emission intensity based on spectrum data from event.

---
+++ TSD_UnityProject/Assets/Scripts/RadioSpectrumVisualizer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioSpectrumVisualizer : MonoBehaviour
+{
+	private SoundEmitter emitter;
+	private Shader emissiveColor;
+	private float emission;
+
+    void Start()
+    {
+	    emitter = GetComponent<SoundEmitter>();
+	    emissiveColor = GetComponent<Shader>();
+    }
+
+    void Update()
+    {
+	    Renderer renderer = GetComponent<Renderer>();
+	    Material mat = renderer.material;
+
+	    emission = (emitter.RtpcData * -1 / 10000) / 2;
+
+	    Color baseColor = new Color(191f,15f,0f);
+	    Color finalColor = baseColor * Mathf.LinearToGammaSpace(emission);
+
+	    mat.SetColor("_EmissionColor", finalColor);
+	}
+}
