1fbddbd76603c1f73b0abb8cfda410bb05f936dd
Filip Conic <filip.conic@rovio.com>
2020-02-28 16:32:03 +0100

    First implementation of wind object.

---
+++ TSD_UnityProject/Assets/Wind.cs
@@ -0,0 +1,21 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+public class Wind : MonoBehaviour
+{
+	private Transform cam;
+	public float radians;
+	public float distance;
+
+	void Start()
+	{
+		cam = gameObject.GetComponentInParent<CinemachineVirtualCamera>().transform;
+	}
+
+	void LateUpdate()
+	{
+		transform.position = new Vector3(cam.position.x + distance * Mathf.Cos(Mathf.Deg2Rad * radians), cam.transform.position.y,
+			cam.position.z + distance *Mathf.Sin(Mathf.Deg2Rad * radians));
+		transform.LookAt(cam);
+	}
+}
